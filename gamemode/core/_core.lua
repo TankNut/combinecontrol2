36d7929b7e0529c413345f42ead754eea690398f
TankNut <m.vlasblom@live.nl>
2024-11-12 00:43:07 +0100

    Add realm-independent include func

--- gamemode/core/_core.lua
+++ gamemode/core/_core.lua
@@ -1,8 +1,20 @@
 -- Starting off fresh
 
+function GM:Include(path)
+	local realm = path:Left(3)
+
+	if realm == "cl_" then
+		return self:IncludeClient(path)
+	elseif realm == "sv_" then
+		return self:IncludeServer(path)
+	end
+
+	return self:IncludeShared(path)
+end
+
 function GM:IncludeClient(path)
 	if CLIENT then
-		include(path)
+		return include(path)
 	else
 		AddCSLuaFile(path)
 	end
@@ -10,31 +22,31 @@ end
 
 function GM:IncludeShared(path)
 	AddCSLuaFile(path)
-	include(path)
+	return include(path)
 end
 
 function GM:IncludeServer(path)
 	if SERVER then
-		include(path)
+		return include(path)
 	end
 end
 
-GM:IncludeShared("sh_helpers.lua")
 
-GM:IncludeShared("sh_player_vars.lua")
 
-GM:IncludeServer("sv_database.lua")
-GM:IncludeShared("sh_admin.lua")
 
-GM:IncludeServer("sv_player.lua")
 
 hook.Add("InitPostEntity", "combinecontrol", function()
 	hook.Run("LoadDatabase")
 end)
+GM:Include("sh_helpers.lua")
+GM:Include("sh_player_vars.lua")
 
 hook.Add("LuapadCanRunCL", "combinecontrol", function(ply)
 	return ply:IsDeveloper()
 end)
+GM:Include("sh_admin.lua")
+GM:Include("sv_player.lua")
+GM:Include("sv_database.lua")
 
 hook.Add("LuapadCanRunSV", "combinecontrol", function(ply)
 	return ply:IsDeveloper()
