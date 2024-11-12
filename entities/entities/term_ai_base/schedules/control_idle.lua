f0ef5a9a36870f0ff626ebdae611c5b6829360aa
TankNut <m.vlasblom@live.nl>
2024-11-12 19:25:55 +0100

    Remove all of the terminator AI code
    Not planning on doing something like that again

--- entities/entities/term_ai_base/schedules/control_idle.lua
+++
@@ -1,9 +0,0 @@
-local schedule = ai_schedule.New("Control_Idle")
-
-schedule:EngTask("TASK_WAIT", 5)
-
-ENT:AddSchedule(schedule, function(self, interrupts)
-	if interrupts.VisibleEnemy then
-		self:SetCustomSchedule("Control_Attack")
-	end
-end)
