2c077f0aaa45cdee9e2801d441558d359e88e011
TankNut <m.vlasblom@live.nl>
2024-11-16 04:06:55 +0100

    Add separate armor fraction tracking

---
+++ gamemode/core/plugins/sv_armor.lua
@@ -0,0 +1,22 @@
+local meta = FindMetaTable("Player")
+
+if not meta._SetMaxArmor then
+	meta._SetMaxArmor = meta.SetMaxArmor
+end
+
+function meta:SetMaxArmor(val)
+	self:_SetMaxArmor(val)
+	self:SetArmor(math.min(self.ArmorFraction * val, val))
+end
+
+hook.Add("PlayerPostThink", "armor", function(ply)
+	local max = ply:GetMaxArmor()
+
+	if max > 0 then
+		ply.ArmorFraction = math.Clamp(ply:Armor() / max, 0, 1)
+	end
+end)
+
+hook.Add("PlayerSpawn", "armor", function(ply)
+	ply.ArmorFraction = 1
+end)
