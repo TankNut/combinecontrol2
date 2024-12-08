9e56a4afb6fc7d7af322a0b55fdf0e717c05f863
TankNut <m.vlasblom@live.nl>
2024-12-08 03:43:18 +0100

    Fix item registration for other items

--- gamemode/core/sh_item.lua
+++ gamemode/core/sh_item.lua
@@ -22,7 +22,7 @@ function Register(name, item)
 
 	if name != "base" then
 		setmetatable(item, {
-			__index = baseclass.Get("item_" .. item.Base or "item_base")
+			__index = baseclass.Get(item.Base and "item_" .. item.Base or "item_base")
 		})
 	end
 
