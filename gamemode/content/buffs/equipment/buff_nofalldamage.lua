27ee07903096512151680998afda7bea0d75f1d8
TankNut <m.vlasblom@live.nl>
2025-01-11 03:44:42 +0100

    Equipment buffs > static buffs

--- gamemode/content/buffs/equipment/buff_nofalldamage.lua
+++
@@ -1,4 +0,0 @@
-DEFINE_BASECLASS("buff_base_equipment")
-
-BUFF.Base = "base_equipment"
-BUFF.BlockFallDamage = true
---
+++ gamemode/content/buffs/static/buff_nofalldamage.lua
@@ -0,0 +1,4 @@
+DEFINE_BASECLASS("buff_base_static")
+
+BUFF.Base = "base_static"
+BUFF.BlockFallDamage = true
