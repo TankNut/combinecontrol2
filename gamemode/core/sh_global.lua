d3f538c017aa02b2351d5df4b97e60acdd67e176
TankNut <m.vlasblom@live.nl>
2025-02-04 23:20:05 +0100

    Remove test global var

--- gamemode/core/sh_global.lua
+++ gamemode/core/sh_global.lua
@@ -2,8 +2,3 @@ GlobalVar.Add("OOCDelay", {Default = 0})
 
 GlobalVar.Add("AprilFools", {Default = os.date("!%d-%m") == "01-04"})
 GlobalVar.Add("Cursed", {Default = 0})
-
-GlobalVar.Add("PersistTest", {
-	Default = false,
-	Persist = true
-})
