33e166277bca091f91d641a981214d595dee2570
TankNut <m.vlasblom@live.nl>
2025-02-08 00:30:12 +0100

    Rename settings files to make them easier to find

--- gamemode/content/_content.lua
+++ gamemode/content/_content.lua
@@ -2,6 +2,7 @@ GM:Include("sh_defines.lua")
 GM:Include("sh_names.lua")
 GM:Include("sh_sandbox.lua")
 
-GM:Include("settings/general.lua")
-GM:Include("settings/hud.lua")
-GM:Include("settings/admin.lua")
+-- Load order determines category order, plugins shouldn't matter too much so we let those sort themselves out
+GM:Include("settings/settings_general.lua")
+GM:Include("settings/settings_hud.lua")
+GM:Include("settings/settings_admin.lua")
