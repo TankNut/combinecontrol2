64d0eb25b9d7e075c133db3762e9a84a3876131f
TankNut <m.vlasblom@live.nl>
2025-02-09 00:46:47 +0100

    Move global vars into sh_global_vars.lua for now

--- gamemode/core/sh_global.lua
+++
@@ -1,4 +0,0 @@
-GlobalVar.Add("OOCDelay", {Default = 0})
-
-GlobalVar.Add("AprilFools", {Default = os.date("!%d-%m") == "01-04"})
-GlobalVar.Add("Cursed", {Default = 0})
--- gamemode/core/sh_global_vars.lua
+++ gamemode/core/sh_global_vars.lua
@@ -122,3 +122,8 @@ else
 		end
 	end
 end
+
+GlobalVar.Add("OOCDelay", {Default = 0})
+
+GlobalVar.Add("AprilFools", {Default = os.date("!%d-%m") == "01-04"})
+GlobalVar.Add("Cursed", {Default = 0})
