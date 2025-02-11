f5340b8d3f37b7245c37c7f9e2a178758c8dfac0
TankNut <m.vlasblom@live.nl>
2025-02-11 01:32:09 +0100

    Remove cl_resource.lua

--- gamemode/cl_resource.lua
+++
@@ -1,9 +0,0 @@
--- Load some particle systems on the client
-game.AddParticles("particles/building_explosion.pcf")
-game.AddParticles("particles/weapon_fx.pcf") -- this one doesn't work?!
-game.AddParticles("particles/hunter_flechette.pcf")
-game.AddParticles("particles/hunter_projectile.pcf")
-game.AddParticles("particles/hunter_shield_impact.pcf")
-game.AddParticles("particles/electrical_fx.pcf")
-game.AddParticles("particles/antlion_gib_01.pcf")
-game.AddParticles("particles/advisor.pcf")
