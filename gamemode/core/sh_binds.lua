52b4688af9cd1959c355aa5de34f477cdb871fcc
TankNut <m.vlasblom@live.nl>
2025-02-12 03:34:14 +0100

    Move binds code

--- gamemode/core/sh_binds.lua
+++ gamemode/core/sh_binds.lua
@@ -1,4 +1,40 @@
-if SERVER then
+if CLIENT then
+	function GM:PlayerBindPress(ply, bind, down)
+		if not ply:HasCharacter() then
+			return true
+		end
+
+		if Chat.Bind(bind, down) then return true end
+		if WeaponSelect.Bind(bind, down) then return true end
+
+		if down and string.find(bind, "rp_toggleholster") then
+			net.Start("nToggleHolster")
+			net.SendToServer()
+
+			local weapon = LocalPlayer():GetActiveWeapon()
+
+			if IsValid(weapon) then
+				if weapon.TRP then
+					RunConsoleCommand("impulse", 30)
+				end
+
+				if weapon.ToggleHolster then
+					weapon:ToggleHolster()
+				end
+
+				if weapon.Holsterable then
+					LocalPlayer():SetHolstered(not LocalPlayer():Holstered())
+				else
+					LocalPlayer():SetHolstered(false)
+				end
+			end
+
+			return true
+		end
+
+		return hook.Run("CC.CL.PlayerBindPress", ply, bind, down)
+	end
+else
 	function GM:ShowHelp(ply)
 		ply:OpenGUI("HelpMenu")
 	end
