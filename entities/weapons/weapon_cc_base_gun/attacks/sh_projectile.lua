71ce4903fedc74e2072428247e0709f7e330bf74
TankNut <m.vlasblom@live.nl>
2025-10-31 02:24:14 +0100

    Add offsets to projectile attacks

--- entities/weapons/weapon_cc_base_gun/attacks/sh_projectile.lua
+++ entities/weapons/weapon_cc_base_gun/attacks/sh_projectile.lua
@@ -11,8 +11,13 @@ function SWEP:FireProjectile(owner)
 	spread = AngleRand(-spread, spread)
 	spread.r = 0
 
-	ent:SetPos(owner:GetShootPos())
-	ent:SetAngles(self:GetShootDir():Angle() + spread)
+	local pos = self.Stats.Offset or vector_origin
+	local ang = self.Stats.Angle or angle_zero
+
+	pos, ang = LocalToWorld(pos, ang + spread, owner:GetShootPos(), self:GetShootDir():Angle())
+
+	ent:SetPos(pos)
+	ent:SetAngles(ang)
 
 	ent:SetOwner(owner)
 	ent:Spawn()
