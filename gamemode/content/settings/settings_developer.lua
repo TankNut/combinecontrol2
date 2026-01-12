3f0d8c1b50bb9038163e5382bf2ec1280a54fc41
TankNut <m.vlasblom@live.nl>
2026-01-13 00:59:28 +0100

    Default RainbowPhysgun to false

--- gamemode/content/settings/settings_developer.lua
+++ gamemode/content/settings/settings_developer.lua
@@ -2,7 +2,7 @@ local isDeveloper = FindMetaTable("Player").IsDeveloper
 
 Settings.Add("RainbowPhysgun", {
 	Name = "Rainbow Physics Gun",
-	Default = true,
+	Default = false,
 	Validate = validate.Bool(),
 	Panel = "CC_Setting_Bool",
 	CanAccess = isDeveloper
